9e8abd0abd848bf5fb878f1f58721b4f0918e2cd
ArielDiasNavia <Ariel.dias.navia@gmail.com>
2017-12-03 23:35:40 -0300

    TP_04
    Recuperatorio Navegador TP4

---
+++ RecuperatoriosTP/Dias.Navia.Emanuel_TP4/Archivos/Texto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _nomArhc;
+
+        /// <summary>
+        /// constructor que recibe el nombre del archivo
+        /// </summary>
+        /// <param name="archivo"></param>
+        public Texto(string archivo)
+        {
+            this._nomArhc = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(this._nomArhc, true);
+                sw.WriteLine(datos);
+
+                sw.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Si no falla, lee todos los datos del Arhivo.
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(this._nomArhc);
+
+                datos = new List<string>();
+
+                while (!sr.EndOfStream)
+                {
+                    datos.Add(sr.ReadLine());
+                }
+
+                sr.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                datos = default(List<string>);
+                return false;
+            }
+        }
+
+    }
+
+}
---
+++ RecuperatoriosTP/Dias.Navia.Emanuel_TP4/Hilo/Descargador.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string _html;
+        private Uri _direccion;
+
+        public delegate void EventProgress(int estado);
+        public event EventProgress evProgress;
+        public delegate void EventCompleted(string web);
+        public event EventCompleted evFinalizado;
+
+        /// <summary>
+        /// Recibe la direccion web
+        /// </summary>
+        /// <param name="direccion"></param>
+        public Descargador(Uri direccion)
+        {
+            this._html = "";
+            this._direccion = direccion;
+        }
+
+        /// <summary>
+        /// inicia la descarga de la pagina web
+        /// </summary>
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this._direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        /// <summary>
+        /// Progreso de descarga
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.evProgress(e.ProgressPercentage);
+        }
+        /// <summary>
+        /// progreso de la descarga finalizada
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this._html = e.Result;
+            }
+            catch (Exception exception)
+            {
+                this._html = exception.Message;
+            }
+            finally
+            {
+                this.evFinalizado(this._html);
+            }
+        }
+    }
+}
